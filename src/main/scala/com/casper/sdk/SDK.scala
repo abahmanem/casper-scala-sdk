8642a052efbd8ef5f28de21983889389dce3317a
Mikael Grouwet <synozwave@gmail.com>
2021-11-04 15:20:19 +0100

    milestone #2

--- src/main/scala/com/casper/sdk/SDK.scala
+++ src/main/scala/com/casper/sdk/SDK.scala
@@ -9,12 +9,21 @@ import scala.reflect.runtime.universe._
 
 class SDK[F[_]](transport: Transport[F]) (implicit me: MonadException[F]) extends Client[F](transport)  {
 
-  // def get_help(data: Seq["Lits all available calls (like unix man command) "]) :  F[String] = call("help", data)
-  // def get_info_get_deploy(data: Seq[String]) :  F[String] = call("info_get_deploy", data)
+  //Milestone #1
   def get_info_get_peers(data: Seq[String]) :  F[GetPeersResult] = call("info_get_peers", data)
   def get_chain_get_state_root_hash(data: Seq[String]) :  F[GetStateRootHashResult] = call("chain_get_state_root_hash", data)
-  def chain_get_block(data: Seq[String]) :  F[GetBlockResult] = call("chain_get_block", data)
-  // def get_state_get_auction_info(data: Seq[String]) :  F[String] = call("state_get_auction_info", data)
+  //Milestone #2
+  // Chad
+  // def chain_get_block(data: Seq[String]) :  F[GetBlockResult] = call("info_get_deploy", data)
+  // def chain_get_block(data: Seq[String]) :  F[GetBlockResult] = call("info_get_status", data)
+  // def chain_get_block(data: Seq[String]) :  F[GetBlockResult] = call("chain_get_block_transfers", data)
+  // def chain_get_block(data: Seq[String]) :  F[GetBlockResult] = call("chain_get_block", data)
+  // def chain_get_block(data: Seq[String]) :  F[GetBlockResult] = call("chain_get_era_info_by_switch_block", data)
+  // Mika
+  // def chain_get_block(data: Seq[String]) :  F[GetBlockResult] = call("state_get_item", data)
+  // def chain_get_block(data: Seq[String]) :  F[GetBlockResult] = call("state_get_dictionary_item", data)
+  // def chain_get_block(data: Seq[String]) :  F[GetBlockResult] = call("state_get_balance", data)
+  // def chain_get_block(data: Seq[String]) :  F[GetBlockResult] = call("state_get_auction_info", data)
 
 
 }
