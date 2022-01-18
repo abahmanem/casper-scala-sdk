839af32857be8589e3a573f4d9b1013f7a0741c5
abahmane <p35862@hepl.ch>
2022-01-18 11:46:03 +0100

    Crypto + refactoring

--- src/main/scala/com/casper/sdk/crypto/SECP256K1.scala
+++ src/main/scala/com/casper/sdk/crypto/SECP256K1.scala
@@ -1,23 +1,19 @@
 package com.casper.sdk.crypto
 
-import org.bouncycastle.asn1.{ASN1InputStream, ASN1Integer, DERSequenceGenerator, DLSequence}
 import org.bouncycastle.asn1.sec.SECNamedCurves
-import org.bouncycastle.asn1.x9.X9ECParameters
-import org.bouncycastle.crypto.AsymmetricCipherKeyPair
+import org.bouncycastle.asn1.{ASN1InputStream, ASN1Integer, DERSequenceGenerator, DLSequence}
 import org.bouncycastle.crypto.digests.SHA256Digest
-import org.bouncycastle.crypto.generators.ECKeyPairGenerator
 import org.bouncycastle.crypto.params.{ECDomainParameters, ECKeyGenerationParameters, ECPrivateKeyParameters, ECPublicKeyParameters}
 import org.bouncycastle.crypto.signers.{ECDSASigner, HMacDSAKCalculator}
 import org.bouncycastle.math.ec.ECPoint
 
 import java.io.{ByteArrayOutputStream, IOException}
-import java.math.BigInteger
+
 
 /**
- *  SECP256K1 object utility
+ * SECP256K1 object utility
  */
 object SECP256K1 {
-
 
   val curve = SECNamedCurves.getByName("secp256k1")
   val domain = new ECDomainParameters(curve.getCurve(), curve.getG(), curve.getN(),
@@ -25,18 +21,17 @@ object SECP256K1 {
 
   /**
    * verify a signature
-   * @param msg signed msg
+   *
+   * @param msg       signed msg
    * @param signature signature to verify
    * @param publicKey key to verifiy msg against
    * @return true if the signature is verified or false if not
    */
   def verify(msg: Array[Byte], signature: Array[Byte], publicKey: Array[Byte]): Boolean = {
 
-    val curve = SECNamedCurves.getByName("secp256k1")
-    val domParams = new ECDomainParameters(curve.getCurve, curve.getG, curve.getN, curve.getH, curve.getSeed)
     val q = curve.getCurve.decodePoint(publicKey)
     val signer = new ECDSASigner()
-    signer.init(false, new ECPublicKeyParameters(q, domParams))
+    signer.init(false, new ECPublicKeyParameters(q, domain))
     val asn1 = new ASN1InputStream(signature)
     try {
       val seq = asn1.readObject().asInstanceOf[DLSequence]
@@ -50,28 +45,29 @@ object SECP256K1 {
     }
   }
 
-    /**
-     * sign a message
-     * @param msg message to sign
-     * @param privateKey privateKey to use t osign the message
-     * @return signature byte array
-     */
-    def sign(msg: Array[Byte], params: ECPrivateKeyParameters): Array[Byte] = {
-      val signer = new ECDSASigner(new HMacDSAKCalculator(new SHA256Digest()))
-      signer.init(true, params)
-      val signature = signer.generateSignature(msg)
-      val baos = new ByteArrayOutputStream()
-      try {
-        val seq: DERSequenceGenerator = new DERSequenceGenerator(baos)
-        seq.addObject(new ASN1Integer(signature(0)))
-        seq.addObject(new ASN1Integer(signature(1)))
-        seq.close
-        baos.toByteArray
-      }
-      catch {
-        case io: IOException => null
-      }
+  /**
+   * sign a message
+   *
+   * @param msg        message to sign
+   * @param privateKey privateKey to use t osign the message
+   * @return signature byte array
+   */
+  def sign(msg: Array[Byte], params: ECPrivateKeyParameters): Array[Byte] = {
+    val signer = new ECDSASigner(new HMacDSAKCalculator(new SHA256Digest()))
+    signer.init(true, params)
+    val signature = signer.generateSignature(msg)
+    val baos = new ByteArrayOutputStream()
+    try {
+      val seq: DERSequenceGenerator = new DERSequenceGenerator(baos)
+      seq.addObject(new ASN1Integer(signature(0)))
+      seq.addObject(new ASN1Integer(signature(1)))
+      seq.close
+      baos.toByteArray
     }
+    catch {
+      case io: IOException => null
+    }
+  }
 }
 
 
