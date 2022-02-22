79d3d5109a8b973ba51f73f89a9808ef31074d2b
abahmane <p35862@hepl.ch>
2022-02-22 09:39:18 +0100

    CL Issues : refactoring on exceptions + tests

--- src/main/scala/com/casper/sdk/serialization/domain/deploy/DeployApprovalByteSerializer.scala
+++ src/main/scala/com/casper/sdk/serialization/domain/deploy/DeployApprovalByteSerializer.scala
@@ -13,7 +13,7 @@ class DeployApprovalByteSerializer extends BytesSerializable[DeployApproval] {
   def toBytes(value: DeployApproval): Array[Byte] = {
     require(value != null)
     val builder = new ArrayBuilder.ofByte
-    //signer and signature are both present
+    //signer and signature are both set or not
     if (value.signature.isDefined && value.signer.isDefined)
       builder.addAll(value.signer.get.formatAsByteAccount).addAll(value.signature.get.formatAsByteAccount).result()
     else
