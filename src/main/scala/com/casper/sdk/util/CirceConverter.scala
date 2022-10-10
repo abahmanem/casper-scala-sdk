ab5b20ad5f8868004a267e3111092efb257b6982
abahmane <p35862@hepl.ch>
2022-10-10 12:16:47 +0200

    Refactoring with Circe library

--- src/main/scala/com/casper/sdk/util/CirceConverter.scala
+++ src/main/scala/com/casper/sdk/util/CirceConverter.scala
@@ -1,25 +1,35 @@
 package com.casper.sdk.util
 
-import com.casper.sdk.rpc.RPCResult
 import io.circe.generic.auto._
-import io.circe.generic.semiauto.deriveDecoder
 import io.circe.{Decoder, Encoder}
 import io.circe.{Decoder, Encoder}
-//import io.circe.generic.extras.auto._
-//import io.circe.generic.extras.Configuration
-//import io.circe.generic.semiauto
-import io.circe.generic.semiauto.deriveEncoder
-import io.circe.generic.semiauto.deriveDecoder
 import io.circe.parser.decode
 import io.circe.syntax._
 import scala.reflect.ClassTag
 import scala.util.{Failure, Success, Try}
 
+/**
+ * Json serializing/deserilizing utils
+ */
 object CirceConverter {
 
 
-
+  /**
+   * encode casper types
+   * @param t
+   * @param encoder
+   * @tparam T
+   * @return Try[t.asJson.noSpaces]
+   */
   def toJson[T:ClassTag](t: T)(implicit encoder: Encoder[T]): Try[String] = Try(t.asJson.noSpaces)
+
+  /**
+   * decode casper types
+   * @param jsonStr
+   * @param decoder
+   * @tparam T
+   * @return Try[T]
+   */
 
   def convertToObj[T](jsonStr: String)(implicit decoder: Decoder[T]): Try[T] =
     decode[T](jsonStr) match {
